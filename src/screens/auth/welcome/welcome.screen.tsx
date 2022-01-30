aef0c66c1e283ab1a738071f5dae3f7a56af924c
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-29 21:24:52 -0300

    feat: add Buttons to Welcome Screen

--- src/screens/auth/welcome/welcome.screen.tsx
+++ src/screens/auth/welcome/welcome.screen.tsx
@@ -1,13 +1,53 @@
 import React, { FunctionComponent } from 'react'
-import { Text } from 'react-native'
+import { Image, StyleSheet } from 'react-native'
+import CustomButton from '~components/common/custom-button/custom-button.component'
+
+// Components
+import TextBold from '~components/common/text-bold/text-bold.component'
+
+// Styles
+import { Buttons, Container, Content } from './welcome.styles'
+
+const styles = StyleSheet.create({
+  button: { marginVertical: 7.5 }
+})
 
 interface WelcomeScreenProps {}
 
 const WelcomeScreen: FunctionComponent<WelcomeScreenProps> = () => {
   return (
-    <Text>
-      Campeonatos do Automobilismo Virtual na palma da sua mão. De graça.
-    </Text>
+    <Container>
+      <Image
+        source={{
+          uri: 'https://sim-racer.s3.sa-east-1.amazonaws.com/unsplash_RNl62zcpSy0.png'
+        }}
+        style={{ resizeMode: 'cover', flex: 1 }}
+      />
+
+      <Content>
+        <TextBold style={{ fontSize: 18, textAlign: 'center' }}>
+          Campeonatos do Automobilismo Virtual na palma da sua mão. De graça.
+        </TextBold>
+
+        <Buttons>
+          <CustomButton variant="primary" style={styles.button}>
+            Cadastre-se gratuitamente
+          </CustomButton>
+
+          <CustomButton variant="outlined" style={styles.button}>
+            Entrar com e-mail e senha
+          </CustomButton>
+
+          <CustomButton variant="outlined" style={styles.button}>
+            Continuar com o Google
+          </CustomButton>
+
+          <CustomButton variant="outlined" style={styles.button}>
+            Continuar com a Apple
+          </CustomButton>
+        </Buttons>
+      </Content>
+    </Container>
   )
 }
 
---
+++ src/screens/auth/welcome/welcome.styles.tsx
@@ -0,0 +1,16 @@
+import styled from 'styled-components/native'
+
+export const Container = styled.View`
+  flex: 1;
+  justify-content: flex-end;
+`
+export const Content = styled.View`
+  padding-bottom: 100px;
+  padding-right: 25px;
+  padding-left: 25px;
+  background-color: #000000;
+`
+
+export const Buttons = styled.View`
+  margin-top: 12px;
+`
