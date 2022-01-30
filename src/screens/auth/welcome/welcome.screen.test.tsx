2778a0e16555c57f40a83d4b31eb600286c8c707
Felipe Rocha <felipemota.rocha@gmail.com>
2022-01-29 21:25:01 -0300

    test: add Buttons to Welcome Screen

--- src/screens/auth/welcome/welcome.screen.test.tsx
+++ src/screens/auth/welcome/welcome.screen.test.tsx
@@ -3,7 +3,9 @@ import { render } from '@testing-library/react-native'
 
 import WelcomeScreen from './welcome.screen'
 
-it('examples of some things', async () => {
+import Colors from '~constants/colors.constants'
+
+it('should show the welcome text and the login buttons', async () => {
   const { getByText } = render(<WelcomeScreen />)
 
   const textElement = getByText(
@@ -11,4 +13,21 @@ it('examples of some things', async () => {
   )
 
   expect(textElement).toBeDefined()
+  expect(textElement).toHaveStyle({
+    fontSize: 18,
+    fontFamily: 'Poppins_700Bold',
+    color: Colors.text
+  })
+
+  const signUpFree = getByText(/cadastre-se gratuitamente/i)
+  expect(signUpFree).toBeDefined()
+
+  const signInWithEmail = getByText(/entrar com e-mail e senha/i)
+  expect(signInWithEmail).toBeDefined()
+
+  const signInWithGoogle = getByText(/continuar com o google/i)
+  expect(signInWithGoogle).toBeDefined()
+
+  const signInWithApple = getByText(/continuar com a apple/i)
+  expect(signInWithApple).toBeDefined()
 })
