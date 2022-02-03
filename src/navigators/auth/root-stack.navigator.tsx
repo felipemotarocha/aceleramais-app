202b1e1b2d207ec2e1d878c2ce1d9b3003057ca9
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-03 20:21:40 -0300

    fix: firebase sign in loop

--- src/navigators/auth/root-stack.navigator.tsx
+++ src/navigators/auth/root-stack.navigator.tsx
@@ -13,19 +13,30 @@ import { useAppSelector } from '~store'
 
 // Redux Actions
 import { loginUser, signOutUser } from '~store/user/user.actions'
+
+// Utilities
 import { auth } from '~config/firebase.config'
+import axios from 'axios'
+import { API_URL } from '~constants/config.constants'
 
 interface RootStackNavigatorProps {}
 
 const RootStackNavigator: FunctionComponent<RootStackNavigatorProps> = () => {
-  const { currentUser } = useAppSelector((state) => state.user)
+  const { currentUser, loading } = useAppSelector((state) => state.user)
+
+  console.log({ loading })
 
   const dispatch = useDispatch()
 
   onAuthStateChanged(auth, async (user) => {
-    if (user && !currentUser) {
-      const authToken = await user.getIdToken()
-      dispatch(loginUser(user.uid, authToken))
+    if (user && !currentUser && !loading) {
+      try {
+        await axios.get(`${API_URL}/api/user?id=${user.uid}`)
+
+        const authToken = await user.getIdToken()
+
+        dispatch(loginUser(user.uid, authToken))
+      } catch (_err) {}
     }
 
     if (!user && currentUser) {
