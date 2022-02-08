e40c96cfd79ddd33b3c515955878a11bcdeaec44
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-08 14:10:06 -0300

    chore: add KeyboardAwareFlatList mock

--- jest.setup.js
+++ jest.setup.js
@@ -1,9 +1,12 @@
 import '@testing-library/jest-native/extend-expect'
 import mockAsyncStorage from '@react-native-async-storage/async-storage/jest/async-storage-mock'
+import { FlatList as mockFlatlist } from 'react-native'
 
 jest.mock('@react-native-async-storage/async-storage', () => mockAsyncStorage)
 
 jest.mock('react-native-keyboard-aware-scroll-view', () => {
   const KeyboardAwareScrollView = ({ children }) => children
-  return { KeyboardAwareScrollView }
+  const KeyboardAwareFlatList = mockFlatlist
+
+  return { KeyboardAwareScrollView, KeyboardAwareFlatList }
 })
