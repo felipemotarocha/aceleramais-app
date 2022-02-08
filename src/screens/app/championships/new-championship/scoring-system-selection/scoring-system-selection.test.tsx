d50d712442ee0cef6b1ee632ed19bf24449e75e3
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-08 14:15:45 -0300

    test: ensure Scoring System Selection shows an error when trying to add points without position

--- src/screens/app/championships/new-championship/scoring-system-selection/scoring-system-selection.test.tsx
+++ src/screens/app/championships/new-championship/scoring-system-selection/scoring-system-selection.test.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { render } from '~helpers/test.helpers'
+import { render, fireEvent, waitFor } from '~helpers/test.helpers'
 
 import ChampionshipScoringSystemSelectionContainer from './scoring-system-selection.container'
 
@@ -13,6 +13,20 @@ describe('Championship Scoring System Selection', () => {
     getByText(/adicionar/i)
     getByText(/avançar/i)
     getByText(/coloque apenas as posições que pontuam/i)
+
+    expect(queryByLabelText(/remove/i)).toBeNull()
+  })
+
+  it('should show an error if trying to add points without providing a position', async () => {
+    const { getByText, queryByLabelText } = render(
+      <ChampionshipScoringSystemSelectionContainer />
+    )
+
+    const addButton = getByText(/adicionar/i)
+
+    fireEvent.press(addButton)
+
+    await waitFor(async () => getByText(/os pontos são obrigatórios/i))
 
     expect(queryByLabelText(/remove/i)).toBeNull()
   })
