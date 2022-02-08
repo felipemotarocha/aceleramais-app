53af027ae256b888435bc511efe3643503c663cd
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-08 14:29:45 -0300

    test: ensure Scoring System Selection add a position score

--- src/screens/app/championships/new-championship/scoring-system-selection/scoring-system-selection.test.tsx
+++ src/screens/app/championships/new-championship/scoring-system-selection/scoring-system-selection.test.tsx
@@ -30,4 +30,31 @@ describe('Championship Scoring System Selection', () => {
 
     expect(queryByLabelText(/remove/i)).toBeNull()
   })
+
+  it('should add a position score', async () => {
+    const {
+      getByText,
+      queryAllByLabelText,
+      getByPlaceholderText,
+      getByLabelText,
+      queryByText
+    } = render(<ChampionshipScoringSystemSelectionContainer />)
+
+    const input = getByPlaceholderText(/pontos 1º lugar/i)
+
+    fireEvent.changeText(input, '25')
+
+    const addButton = getByText(/adicionar/i)
+
+    fireEvent.press(addButton)
+
+    await waitFor(async () => {
+      getByLabelText(/remove 1/i)
+      getByText(/1º lugar/i)
+      getByPlaceholderText(/pontos 2º lugar/i)
+    })
+
+    expect(queryAllByLabelText(/remove/i)).toHaveLength(1)
+    expect(queryByText(/os pontos são obrigatórios/i)).toBeNull()
+  })
 })
