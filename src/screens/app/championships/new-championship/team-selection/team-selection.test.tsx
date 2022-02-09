7039c15b2aca2c8ee9cba6f25886f2f13243c3b4
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-09 16:43:25 -0300

    test: ensure Championship Team Selection add a Team with default color

--- src/screens/app/championships/new-championship/team-selection/team-selection.test.tsx
+++ src/screens/app/championships/new-championship/team-selection/team-selection.test.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
-import { render } from '~helpers/test.helpers'
+import { render, fireEvent, waitFor } from '~helpers/test.helpers'
+
 import ChampionshipTeamSelectionContainer from './team-selection.container'
 
 describe('Championship Team Selection', () => {
@@ -11,5 +12,20 @@ describe('Championship Team Selection', () => {
     getByText(/selecionar times/i)
     getByText(/adicionar/i)
     getByPlaceholderText(/nome/i)
+  })
+
+  it('should add a Team with default color', async () => {
+    const { getByText, getByPlaceholderText, getByLabelText } = render(
+      <ChampionshipTeamSelectionContainer />
+    )
+
+    fireEvent.changeText(getByPlaceholderText(/nome/i), 'Mercedes')
+
+    fireEvent.press(getByText(/adicionar/i))
+
+    await waitFor(() => {
+      getByText(/mercedes/i)
+      getByLabelText(/remove mercedes/i)
+    })
   })
 })
