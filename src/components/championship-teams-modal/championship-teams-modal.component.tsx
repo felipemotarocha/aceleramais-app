d0da6ac501e1a2d2348db9b5c480e05ac8b3db3e
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-10 19:30:05 -0300

    feat: add Championship Teams Modal Component

---
+++ src/components/championship-teams-modal/championship-teams-modal.component.tsx
@@ -0,0 +1,110 @@
+import React, { FunctionComponent, useCallback, useMemo } from 'react'
+import { Controller, useForm } from 'react-hook-form'
+import { FlatList, Pressable, StyleSheet, View } from 'react-native'
+
+// Components
+import CustomModal from '~components/common/custom-modal/custom-modal.component'
+import TextMedium from '~components/common/text-medium/text-medium.component'
+import CustomInput from '~components/common/custom-input/custom-input.component'
+
+import { _Team } from '~store/championship-creation/championship-creation.slice'
+
+interface ChampionshipTeamsModalProps {
+  teams: _Team[]
+  isVisible: boolean
+  setIsVisible: React.Dispatch<React.SetStateAction<boolean>>
+  handleTeamChange: (team: _Team) => void
+}
+
+const ChampionshipTeamsModal: FunctionComponent<
+  ChampionshipTeamsModalProps
+> = ({ teams, isVisible, setIsVisible, handleTeamChange }) => {
+  const { control, watch } = useForm<{ filter: string }>({ mode: 'onChange' })
+
+  const watchFilter = watch('filter')
+
+  const filteredTeams = useMemo(() => {
+    if (!watchFilter) return teams
+
+    return teams.filter((team) =>
+      team.name.toLowerCase().startsWith(watchFilter.toLowerCase())
+    )
+  }, [watchFilter])
+
+  const handlePress = useCallback((team: _Team) => {
+    handleTeamChange(team)
+    setIsVisible(false)
+  }, [])
+
+  const renderItem = useCallback(
+    ({ item }: { item: _Team }) => (
+      <Pressable
+        onPress={() => handlePress(item)}
+        style={{
+          flexDirection: 'row',
+          alignItems: 'center',
+          marginVertical: 20
+        }}>
+        <View style={[styles.circle, { borderColor: item.color }]}></View>
+
+        <View style={{ flexDirection: 'row', alignItems: 'center' }}>
+          <View style={[styles.colorLine, { borderColor: item.color }]}></View>
+          <TextMedium style={{ fontSize: 12 }}>{item.name}</TextMedium>
+        </View>
+      </Pressable>
+    ),
+    []
+  )
+
+  return (
+    <CustomModal
+      isVisible={isVisible}
+      setIsVisible={setIsVisible}
+      title="Selecionar Time">
+      <View style={styles.container}>
+        <Controller
+          control={control}
+          name="filter"
+          render={({ field: { onChange, value, onBlur } }) => (
+            <CustomInput
+              placeholder="Buscar time..."
+              onChangeText={onChange}
+              value={value}
+              onBlur={onBlur}
+            />
+          )}
+        />
+
+        <FlatList
+          style={{ zIndex: 10 }}
+          contentContainerStyle={{ zIndex: 10 }}
+          data={filteredTeams}
+          renderItem={renderItem}
+          showsVerticalScrollIndicator={false}
+        />
+      </View>
+    </CustomModal>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    padding: 20,
+    paddingBottom: 0
+  },
+  circle: {
+    width: 45,
+    height: 45,
+    borderRadius: 45,
+    borderWidth: 3
+  },
+  colorLine: {
+    borderRadius: 5,
+    borderWidth: 2,
+    marginHorizontal: 8,
+    height: 15
+  }
+})
+
+export default ChampionshipTeamsModal
