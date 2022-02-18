e44d0d43471c4467d36d76f769279bfb4908784e
Felipe Rocha <felipemota.rocha@gmail.com>
2022-02-18 17:54:13 -0300

    test: ensure Championship Details Screen render the next races

--- src/screens/app/championships/championship-details/championship-details.test.tsx
+++ src/screens/app/championships/championship-details/championship-details.test.tsx
@@ -7,18 +7,38 @@ import ChampionshipDetailsContainer from './championship-details.container'
 import ChampionshipDetailsStubs from './championship-details.stubs'
 
 describe('Championship Details', () => {
-  beforeEach(() => {
+  beforeEach(() => {})
+
+  it('should render the header', async () => {
     ;(axiosMock.get as any).mockResolvedValue({
       data: ChampionshipDetailsStubs.validResponse
     })
-  })
 
-  it('should render the header', async () => {
     const { getByText } = render(<ChampionshipDetailsContainer />)
 
     await waitFor(async () => getByText(/psgl sim racing league/i))
     await waitFor(async () => getByText(/plataforma/i))
     await waitFor(async () => getByText(/descrição/i))
     await waitFor(async () => getByText(/join us./i))
+  })
+
+  it('should render the next races', async () => {
+    ;(axiosMock.get as any).mockResolvedValue({
+      data: ChampionshipDetailsStubs.validResponse
+    })
+
+    const { getByText } = render(<ChampionshipDetailsContainer />)
+
+    await waitFor(async () => {
+      getByText(/próximas corridas/i)
+
+      getByText(/melbourne grand prix circuit/i)
+      getByText('01/02/2022, 16:52')
+
+      getByText(/autódromo josé carlos pace/i)
+      getByText('02/02/2022, 16:52')
+
+      getByText(/ver todas as corridas/i)
+    })
   })
 })
