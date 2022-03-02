a6b6d8aeec58215a11d5b69a29f639cc8bb965d2
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-02 17:46:42 -0300

    feat: add helper to generate the drivers available for selection

---
+++ src/components/race-drivers-selection-modal/race-drivers-selection-modal.helper.ts
@@ -0,0 +1,39 @@
+import { ChampionshipDriver } from '~types/championship.types'
+import { RaceClassificationItem } from '~types/race.types'
+
+const RaceDriversSelectionModalHelper = {
+  generateDriversAvailableForSelection: (
+    championshipDrivers: ChampionshipDriver[],
+    raceClassification: RaceClassificationItem[]
+  ) => {
+    const drivers: RaceClassificationItem[] = []
+
+    for (const driver of championshipDrivers) {
+      const driverOnRaceClassification = raceClassification.find((item) =>
+        item.isRegistered
+          ? item?.user?.id === driver?.user?.id
+          : item?.id === driver?.id
+      )
+
+      if (driverOnRaceClassification) {
+        drivers.push(driverOnRaceClassification)
+
+        continue
+      }
+
+      drivers.push({
+        ...driver,
+        position: 0
+      })
+    }
+
+    return drivers.sort((a, b) => {
+      if (a.position === 0) return 1
+      if (b.position === 0) return -1
+
+      return a.position - b.position
+    })
+  }
+}
+
+export default RaceDriversSelectionModalHelper
