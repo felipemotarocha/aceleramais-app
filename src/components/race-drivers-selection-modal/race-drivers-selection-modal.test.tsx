f17716fed3e0210328f90f5d0ce4e23f2795259c
Felipe Rocha <felipemota.rocha@gmail.com>
2022-03-12 16:38:42 -0300

    test: ensure Race Drivers Selection Modal is rendered with a Race Classification

--- src/components/race-drivers-selection-modal/race-drivers-selection-modal.test.tsx
+++ src/components/race-drivers-selection-modal/race-drivers-selection-modal.test.tsx
@@ -5,42 +5,95 @@ import { render, waitFor } from '~helpers/test.helpers'
 import RaceDriversSelectionModalContainer from './race-drivers-selection-modal.container'
 
 describe('Race Drivers Selection Modal', () => {
+  const raceClassification = {
+    id: '622bf07c3e649a39e43234a5',
+    championship: '622bedfbe669549ffd44d2ba',
+
+    race: {
+      championship: '622bedfbe669549ffd44d2ba',
+      track: {
+        name: 'Bahrain International Circuit',
+        countryCode: 'BH',
+        countryName: 'Bahrain',
+        id: '61fdd80633035841deb311e9'
+      },
+      startDate: '2022-03-12T00:46:40.596Z',
+      isCompleted: true,
+      id: '622bedfbe669549ffd44d2ce'
+    },
+    classification: [
+      {
+        position: 1,
+        user: {
+          firstName: 'Felipe',
+          lastName: 'Rocha',
+          userName: 'felipe.rocha',
+          profileImageUrl:
+            'https://sim-racer-app.s3.sa-east-1.amazonaws.com/profile-images/YaWOZoE596acPgLsg7CrAChrQjT2.jpeg',
+          id: 'YaWOZoE596acPgLsg7CrAChrQjT2'
+        },
+        team: {
+          championship: '622bedfbe669549ffd44d2ba',
+          name: 'Mercedes',
+          color: '#03BFB5',
+          id: '622bedfbe669549ffd44d2bb'
+        },
+        isRegistered: true
+      },
+      {
+        position: 2,
+        id: 'afbf4b0f-26d8-443d-bcba-50009edcb216',
+        firstName: 'Max',
+        lastName: 'Verstappen',
+        team: {
+          championship: '622bedfbe669549ffd44d2ba',
+          name: 'Red Bull',
+          color: '#002776',
+          id: '622bedfbe669549ffd44d2bc'
+        },
+        isRegistered: false
+      }
+    ]
+  }
+
+  const drivers = [
+    {
+      user: {
+        firstName: 'Felipe',
+        lastName: 'Rocha',
+        userName: 'felipe.rocha',
+        profileImageUrl:
+          'https://sim-racer-app.s3.sa-east-1.amazonaws.com/profile-images/YaWOZoE596acPgLsg7CrAChrQjT2.jpeg',
+        id: 'YaWOZoE596acPgLsg7CrAChrQjT2'
+      },
+      team: {
+        name: 'Mercedes',
+        color: '#03BFB5',
+        id: '622bedfbe669549ffd44d2bb'
+      },
+      isRegistered: true,
+      bonifications: [],
+      penalties: []
+    },
+    {
+      id: 'afbf4b0f-26d8-443d-bcba-50009edcb216',
+      firstName: 'Max',
+      lastName: 'Verstappen',
+      team: {
+        name: 'Red Bull',
+        color: '#002776',
+        id: '622bedfbe669549ffd44d2bc'
+      },
+      isRegistered: false,
+      bonifications: [],
+      penalties: []
+    }
+  ]
+
   it('should render with no race classification', async () => {
     ;(axiosMock.get as any).mockResolvedValue({
       data: {
-        drivers: [
-          {
-            user: {
-              firstName: 'Felipe',
-              lastName: 'Rocha',
-              userName: 'felipe.rocha',
-              profileImageUrl:
-                'https://sim-racer-app.s3.sa-east-1.amazonaws.com/profile-images/YaWOZoE596acPgLsg7CrAChrQjT2.jpeg',
-              id: 'YaWOZoE596acPgLsg7CrAChrQjT2'
-            },
-            team: {
-              name: 'Mercedes',
-              color: '#03BFB5',
-              id: '622bedfbe669549ffd44d2bb'
-            },
-            isRegistered: true,
-            bonifications: [],
-            penalties: []
-          },
-          {
-            id: 'afbf4b0f-26d8-443d-bcba-50009edcb216',
-            firstName: 'Max',
-            lastName: 'Verstappen',
-            team: {
-              name: 'Red Bull',
-              color: '#002776',
-              id: '622bedfbe669549ffd44d2bc'
-            },
-            isRegistered: false,
-            bonifications: [],
-            penalties: []
-          }
-        ]
+        drivers
       }
     })
 
@@ -64,6 +117,34 @@ describe('Race Drivers Selection Modal', () => {
       getByText('VERSTAPPEN')
 
       expect(queryByText('2º')).toBeNull()
+    })
+  })
+
+  it('should render with a race classification', async () => {
+    ;(axiosMock.get as any).mockResolvedValue({
+      data: {
+        drivers
+      }
+    })
+
+    const { getByText } = render(
+      <RaceDriversSelectionModalContainer
+        championship="622bedfbe669549ffd44d2ba"
+        isVisible
+        raceClassification={raceClassification as any}
+        setIsVisible={() => {}}
+      />
+    )
+
+    await waitFor(async () => {
+      getByText('Felipe')
+      getByText('ROCHA')
+      getByText(/@felipe.rocha/i)
+      getByText('1º')
+
+      getByText('Max')
+      getByText('VERSTAPPEN')
+      getByText('2º')
     })
   })
 })
