f145d8bf4b97336aadcb1266d29b562c66385d08
Felipe Rocha <felipemota.rocha@gmail.com>
2022-04-30 12:51:49 -0300

    feat: add Search Actions

---
+++ src/store/search/search.actions.ts
@@ -0,0 +1,34 @@
+import { Dispatch } from '@reduxjs/toolkit'
+import api from '~api/axios.api'
+
+// Utilities
+import Championship from '~types/championship.types'
+import User from '~types/user.types'
+
+// Redux
+import {
+  SearchEntity,
+  submitSearchFailure,
+  submitSearchStart,
+  submitSearchSuccess
+} from './search.slice'
+
+export const submitSearch = (searchText: string, entity: SearchEntity) => {
+  return async (dispatch: Dispatch) => {
+    await dispatch(submitSearchStart())
+
+    try {
+      let data: Championship[] | User[] = []
+
+      if (entity === 'championship') {
+        data = await api.get(`/api/championship?nameOrCode=${searchText}`)
+      }
+
+      // TODO: add use case for user
+
+      await dispatch(submitSearchSuccess(data))
+    } catch (error: any) {
+      await dispatch(submitSearchFailure(error?.message))
+    }
+  }
+}
