af230d6082b62c99aa42d150fbd155f4ec525358
Felipe Rocha <felipemota.rocha@gmail.com>
2022-05-01 12:05:12 -0300

    test: ensure Search Screen can search Championships by name and code

--- src/screens/app/search/search/search.screen.test.tsx
+++ src/screens/app/search/search/search.screen.test.tsx
@@ -1,13 +1,79 @@
 import * as React from 'react'
-import { render } from '~helpers/test.helpers'
+
+import MockHelpers from '~helpers/mock.helpers'
+import { render, fireEvent, waitFor } from '~helpers/test.helpers'
+import ChampionshipStubs from '~stubs/championship.stubs'
+
 import SearchScreen from './search.screen'
 
 describe('Search Screen', () => {
-  it('should render initial screen', async () => {
+  const axiosMock = MockHelpers.generateAxiosMock()
+
+  it('should render initial screen', () => {
     const { getByText, getByPlaceholderText } = render(<SearchScreen />)
 
     getByText(/pesquisar/i)
     getByPlaceholderText(/nome ou código do campeonato/i)
     getByText(/não há campeonatos para exibir/i)
+  })
+
+  it('should search championships by name', async () => {
+    const { getByPlaceholderText, getByDisplayValue, getByText } = render(
+      <SearchScreen />
+    )
+
+    fireEvent.changeText(
+      getByPlaceholderText(/nome ou código do campeonato/i),
+      'psgl'
+    )
+
+    axiosMock
+      .onGet('/api/championship?nameOrCode=psgl')
+      .reply(200, ChampionshipStubs.validChampionships)
+
+    fireEvent(getByDisplayValue(/psgl/i), 'submitEditing')
+
+    await waitFor(async () => {
+      getByText(/psgl sim racing league/i)
+
+      getByText(/código:/i)
+      getByText(/#12345678/i)
+
+      getByText(/plataforma:/i)
+      getByText(/pc/i)
+
+      getByText(/próxima corrida:/i)
+      getByText('19/04/2022, 15:52')
+    })
+  })
+
+  it('should search championships by code', async () => {
+    const { getByPlaceholderText, getByDisplayValue, getByText } = render(
+      <SearchScreen />
+    )
+
+    fireEvent.changeText(
+      getByPlaceholderText(/nome ou código do campeonato/i),
+      '#123'
+    )
+
+    axiosMock
+      .onGet('/api/championship?nameOrCode=123')
+      .reply(200, ChampionshipStubs.validChampionships)
+
+    fireEvent(getByDisplayValue(/123/i), 'submitEditing')
+
+    await waitFor(async () => {
+      getByText(/psgl sim racing league/i)
+
+      getByText(/código:/i)
+      getByText(/#12345678/i)
+
+      getByText(/plataforma:/i)
+      getByText(/pc/i)
+
+      getByText(/próxima corrida:/i)
+      getByText('19/04/2022, 15:52')
+    })
   })
 })
