06646c93ee0ec71afd452563874cd6fdf30f9169
Felipe Rocha <felipemota.rocha@gmail.com>
2022-05-05 19:10:09 -0300

    feat: add User stubs

---
+++ src/stubs/user.stubs.ts
@@ -0,0 +1,16 @@
+import User from '~types/user.types'
+
+const UserStubs: { validUser: User } = {
+  validUser: {
+    id: 'ohYViVrxyCakFXTrUdlS981v8Pf2',
+    firstName: 'Felipe',
+    lastName: 'Rocha',
+    email: 'felipemotarocha@gmail.com',
+    userName: 'felipe.rocha',
+    profileImageUrl:
+      'https://lh3.googleusercontent.com/a-/AOh14Gg-QreTOcdpYlm9Jys5-NnkxK1gXhoxm-X53FLwcQ=s96-c',
+    provider: 'google'
+  }
+}
+
+export default UserStubs
