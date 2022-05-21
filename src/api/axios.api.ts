9b666b629e89b5c078ff071a8ae74c20d7459101
Felipe Rocha <felipemota.rocha@gmail.com>
2022-05-21 16:33:23 -0300

    feat: add auth interceptor to api

--- src/api/axios.api.ts
+++ src/api/axios.api.ts
@@ -1,8 +1,24 @@
 import axios from 'axios'
+import AsyncStorage from '@react-native-async-storage/async-storage'
 import { API_URL } from '~constants/config.constants'
 
 const api = axios.create({
   baseURL: API_URL
 })
+
+api.interceptors.request.use(
+  async (config) => {
+    const authToken = await AsyncStorage.getItem('authToken')
+
+    if (authToken) {
+      config.headers!.Authorization = `Bearer ${authToken}`
+    }
+
+    return config
+  },
+  (error) => {
+    return Promise.reject(error)
+  }
+)
 
 export default api
