b8aa2a686cd4702351bfc66481a5dd5eecc65e79
Felipe Rocha <felipemota.rocha@gmail.com>
2022-06-02 14:50:02 -0300

    chore: add development environment configs

--- src/config/env.config.ts
+++ src/config/env.config.ts
@@ -1,10 +1,10 @@
 import * as Updates from 'expo-updates'
 
 export const getEnvironment = () => {
-  if (Updates.releaseChannel.startsWith('prd')) {
+  if (Updates.releaseChannel.startsWith('production')) {
     return {
       env: 'production',
-      API_URL: 'http://192.168.15.19:5050',
+      API_URL: 'https://aceleramais-production.herokuapp.com',
       FIREBASE_CONFIG: {
         apiKey: 'AIzaSyCjWamIffDjvLSoFoY6jR9Py4dMrjB5yc0',
         authDomain: 'aceleramais-94101.firebaseapp.com',
@@ -19,6 +19,26 @@ export const getEnvironment = () => {
       ANDROID_CLIENT_ID:
         '20063332770-7kh2sqehdga65f8kgqok735bnqvhdv22.apps.googleusercontent.com',
       AWS_CLOUDFRONT_URL: 'd3g6qzm2mux0or.cloudfront.net'
+    }
+  }
+
+  if (Updates.releaseChannel.startsWith('development')) {
+    return {
+      env: 'development',
+      API_URL: 'https://aceleramais-development.herokuapp.com',
+      GOOGLE_EXPO_CLIENT_ID:
+        '878035662808-nr3s966ip9d6i2u2958a5ad2md0koo2n.apps.googleusercontent.com',
+      FIREBASE_CONFIG: {
+        apiKey: 'AIzaSyCiPCh7T7RO9HwljOsPAOw8sczypuhc7qU',
+        authDomain: 'aceleramais-development.firebaseapp.com',
+        projectId: 'aceleramais-development',
+        storageBucket: 'aceleramais-development.appspot.com',
+        messagingSenderId: '878035662808',
+        appId: '1:878035662808:web:8be3b5e29d7876670b64ad'
+      },
+      ANDROID_CLIENT_ID:
+        '878035662808-v5tadrcl52kkld3tci4tqqfstdk6ueia.apps.googleusercontent.com',
+      AWS_CLOUDFRONT_URL: 'dtz9w99dxalws.cloudfront.net'
     }
   }
 
